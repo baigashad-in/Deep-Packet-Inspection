d7d2362ed048032cf5cbbfc7e42b47ca7f4d9f97
baigashad-in <baigashad.in@gmail.com>
2026-05-28 15:09:08 +0100

    refactor: group app domains into arrays for maintainability
    Replace if/else chain with data-driven AppDomainMap tuple array.
    Adding a new domain = adding one string, no logic changes.
    Remove duplicate AppDomains dictionary and LookupOrder array.
    SniToAppType reduced to 10-line loop over AppDomainMap.

--- Types/AppClassifier.cs
+++ Types/AppClassifier.cs
@@ -9,107 +9,6 @@ namespace Deep_Packet_Analyzer.Types
     public static class AppClassifier
     // Maps domain names(from SNI or HTTP Host headers) to AppType enums.
     {
-
-        private static readonly Dictionary<AppType, string[]> AppDomains = new()
-        {
-            [AppType.YouTube] = new[]
-            {
-                "youtube.com", "ytimg.com", "youtu.be",
-                "googlevideo.com", "yt3.ggpht.com"
-            },
-            [AppType.Google] = new[]
-            {
-                "google.com", "gstatic.com", "googleapis.com",
-                "google.co.in", "gvt1.com", "googlesyndication.com",
-                "googleadservices.com", "googleusercontent.com",
-                "doubleclick.net"
-            },
-            [AppType.Instagram] = new[]
-            {
-                "instagram.com", "cdninstagram.com"
-            },
-            [AppType.WhatsApp] = new[]
-            {
-                "whatsapp.net", "whatsapp.com", "wa.me"
-            },
-            [AppType.Facebook] = new[]
-            {
-                "facebook.com", "fbcdn.net", "fb.com", "meta.com"
-            },
-            [AppType.Twitter] = new[]
-            {
-                "twitter.com", "twimg.com", "x.com", "t.co"
-            },
-            [AppType.Netflix] = new[]
-            {
-                "netflix.com", "nflxvideo.net"
-            },
-            [AppType.Amazon] = new[]
-            {
-                "amazon.com", "amazonaws.com", "cloudfront.net"
-            },
-            [AppType.Microsoft] = new[]
-            {
-                "microsoft.com", "azure.com", "office.com",
-                "live.com", "office365.com"
-            },
-            [AppType.Apple] = new[]
-            {
-                "apple.com", "icloud.com"
-            },
-            [AppType.TikTok] = new[]
-            {
-                "tiktok.com", "bytedance.com", "tiktokcdn.com"
-            },
-            [AppType.Spotify] = new[]
-            {
-                "spotify.com", "scdn.co"
-            },
-            [AppType.Discord] = new[]
-            {
-                "discord.com", "discordapp.com", "discord.gg"
-            },
-            [AppType.GitHub] = new[]
-            {
-                "github.com", "githubusercontent.com"
-            },
-            [AppType.Telegram] = new[]
-            {
-                "telegram.org", "t.me"
-            },
-            [AppType.Zoom] = new[]
-            {
-                "zoom.us", "zoom.com"
-            },
-            [AppType.Cloudflare] = new[]
-            {
-                "cloudflare.com", "cloudflare.net"
-            }
-        };
-
-
-        // Order matters: check specific apps before their parent companies
-        private static readonly AppType[] LookupOrder = new[]
-        {
-            AppType.YouTube,     // Before Google (YouTube is Google subsidiary)
-            AppType.Google,
-            AppType.Instagram,   // Before Facebook (Instagram is Meta subsidiary)
-            AppType.WhatsApp,    // Before Facebook (WhatsApp is Meta subsidiary)
-            AppType.Facebook,
-            AppType.Twitter,
-            AppType.Netflix,
-            AppType.Amazon,
-            AppType.Microsoft,
-            AppType.Apple,
-            AppType.TikTok,
-            AppType.Spotify,
-            AppType.Discord,
-            AppType.GitHub,
-            AppType.Telegram,
-            AppType.Zoom,
-            AppType.Cloudflare
-        };
-
         private static bool MatchesDomain(string host, string domain)
         {
             return host.Equals(domain, StringComparison.OrdinalIgnoreCase) ||
@@ -148,18 +47,79 @@ namespace Deep_Packet_Analyzer.Types
             };
         }
 
+        private static readonly (AppType App, string[] Domains)[] AppDomainMap =
+        {
+            // Check order matters: specific apps before their parent companies
+            (AppType.YouTube, new[] {
+                "youtube.com", "ytimg.com", "youtu.be",
+                "googlevideo.com", "yt3.ggpht.com"
+            }),
+            (AppType.Google, new[] {
+                "google.com", "gstatic.com", "googleapis.com",
+                "google.co.in", "gvt1.com", "googlesyndication.com",
+                "googleadservices.com", "googleusercontent.com",
+                "doubleclick.net"
+            }),
+            (AppType.Instagram, new[] {
+                "instagram.com", "cdninstagram.com"
+            }),
+            (AppType.WhatsApp, new[] {
+                "whatsapp.net", "whatsapp.com", "wa.me"
+            }),
+            (AppType.Facebook, new[] {
+                "facebook.com", "fbcdn.net", "fb.com", "meta.com"
+            }),
+            (AppType.Twitter, new[] {
+                "twitter.com", "twimg.com", "x.com", "t.co"
+            }),
+            (AppType.Netflix, new[] {
+                "netflix.com", "nflxvideo.net"
+            }),
+            (AppType.Amazon, new[] {
+                "amazon.com", "amazonaws.com", "cloudfront.net"
+            }),
+            (AppType.Microsoft, new[] {
+                "microsoft.com", "azure.com", "office.com",
+                "live.com", "office365.com"
+            }),
+            (AppType.Apple, new[] {
+                "apple.com", "icloud.com"
+            }),
+            (AppType.TikTok, new[] {
+                "tiktok.com", "bytedance.com", "tiktokcdn.com"
+            }),
+            (AppType.Spotify, new[] {
+                "spotify.com", "scdn.co"
+            }),
+            (AppType.Discord, new[] {
+                "discord.com", "discordapp.com", "discord.gg"
+            }),
+            (AppType.GitHub, new[] {
+                "github.com", "githubusercontent.com"
+            }),
+            (AppType.Telegram, new[] {
+                "telegram.org", "t.me"
+            }),
+            (AppType.Zoom, new[] {
+                "zoom.us", "zoom.com"
+            }),
+            (AppType.Cloudflare, new[] {
+                "cloudflare.com", "cloudflare.net"
+            }),
+        };
+
         public static AppType SniToAppType(string sni)
         // Classify an SNI hostname into an app category.
         // Called every time we extract an SNI from a TLS Client Hello.
         {
             if (string.IsNullOrEmpty(sni)) return AppType.Unknown;
 
-            foreach (var appType in LookupOrder)
+            foreach (var (app, domains) in AppDomainMap)
             {
-                foreach (var domain in AppDomains[appType])
+                foreach (var domain in domains)
                 {
                     if (MatchesDomain(sni, domain))
-                        return appType;
+                        return app;
                 }
             }
 
